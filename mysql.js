1e154551e74d4ca449b7f67141d2f78fbd94c5d0
moonjjj <mjesun1@naver.com>
2021-03-31 10:05:03 +0900

    commit

--- mysql.js
+++ mysql.js
@@ -13,6 +13,21 @@ connection.query('SELECT * FROM my_db.Users;', function(err, results, fields) {
     console.log(err);
   }
   console.log(results);
+  
 });
 
 connection.end();
+
+
+// var http = require('http');
+// var fs = require('fs');
+// var app = http.createServer(function(request,response){
+//     var url = request.url;
+//     if(url == '/'){
+//       url = '/index.html';
+//     }
+//     response.writeHead(200);
+//     response.end(fs.readFileSync(__dirname + url));
+
+// });
+// app.listen(3000);
--- nodejs.js
+++ nodejs.js
@@ -1,30 +1,3 @@
-// const express    = require('express');
-// const mysql      = require('mysql');
-// const dbconfig   = require('./config/database.js');
-// const connection = mysql.createConnection(dbconfig);
-
-// const app = express();
-
-// // configuration =========================
-// app.set('port', process.env.PORT || 3000);
-
-// app.get('/', (req, res) => {
-//   res.send('Root');
-// });
-
-// app.get('/users', (req, res) => {
-//   connection.query('SELECT * from Users', (error, rows) => {
-//     if (error) throw error;
-//     console.log('User info is: ', rows);
-//     res.send(rows);
-//   });
-// });
-
-// app.listen(app.get('port'), () => {
-//   console.log('Express server listening on port ' + app.get('port'));
-// });
-
-
 // mysql dbms연결
 var mysql = require('mysql');
 var client = mysql.createConnection({
@@ -89,3 +62,4 @@ app.all('/modifyById',function(request,response){
 http.createServer(app).listen(80,function(){
     console.log('Server Running at http://127.0.0.1');
 });
+
